5e06112fe3c331722bdf556642a93364193f6138
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-29 23:23:20 +0200

    add search

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -7,38 +7,31 @@ import { contactFilter } from '../../redux/filtersSlice';
 const SearchBox = () => {
   const dispatch = useDispatch();
 
-  // const handleClear = () => {
-  //   contactFilter('');
-  // };
+  const handleClear = () => {
+    document.querySelector('input[name="username"]').value = '';
+    dispatch(contactFilter(''));
+  };
   const handleSubmit = e => {
     const filterAction = contactFilter(e.target.value);
     dispatch(filterAction);
   };
 
   return (
-    <>
-      <Formik>
-        <Form className={css.container}>
-          <label className={css.text} htmlFor="username">
-            Find contact by name
-          </label>
-          <Field
-            className={css.input}
-            type="text"
-            name="username"
-            onChange={handleSubmit}
-          ></Field>
+    <div className={css.container}>
+      <label className={css.text} htmlFor="username">
+        Find contact by name
+      </label>
+      <input
+        className={css.input}
+        type="text"
+        name="username"
+        onChange={handleSubmit}
+      />
 
-          {/* <button
-            type="button"
-            className={css.clearButton}
-            onClick={handleClear}
-          >
-            Clear
-          </button> */}
-        </Form>
-      </Formik>
-    </>
+      <button type="button" className={css.clearButton} onClick={handleClear}>
+        Clear
+      </button>
+    </div>
   );
 };
 export default SearchBox;
